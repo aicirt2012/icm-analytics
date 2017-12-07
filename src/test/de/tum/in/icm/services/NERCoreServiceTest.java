f40097c013e53f5f8a4c4273947c10773e27a49a
Simon Bönisch <github@sunborn.me>
2017-12-07 02:11:09 +0100

    fixed test

--- src/test/de/tum/in/icm/services/NERCoreServiceTest.java
+++ src/test/de/tum/in/icm/services/NERCoreServiceTest.java
@@ -1,22 +1,17 @@
-package de.tum.in.icm.controllers;
+package de.tum.in.icm.services;
 
 
-import de.tum.in.icm.controllers.CoreNERController;
-import de.tum.in.icm.dtos.NERInputDTO;
 import org.junit.Test;
 
-public class CoreNERControllerTest {
+public class NERCoreServiceTest {
 
-    private CoreNERController testObject = new CoreNERController();
+    private NERCoreService testObject = new NERCoreService();
 
     @Test
     public void Test() {
         String input = "Gespräch  Cable News Network is an American basic cable  Atmosphäre and? satellite television news channel owned by" +
                 " the Turner Broadcasting System, a division of Time Warner." +
                 " CNN was founded in 1980 by American media proprietor Ted !Turner as a Weihnachtsmarkt  24-hour cable news channe";
-        NERInputDTO objectDTO = new NERInputDTO();
-        objectDTO.htmlSource = "<div>test1, test2</div>";
-        testObject.recognize(objectDTO);
         testObject.doRecognize(input);
     }
 }
