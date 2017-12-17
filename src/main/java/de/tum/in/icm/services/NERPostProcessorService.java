eeba631dd2ff0196a51571a68b2f927cc37690fe
Simon Bönisch <github@sunborn.me>
2017-12-17 22:03:17 +0100

    refactored

--- src/main/java/de/tum/in/icm/services/NERPostProcessorService.java
+++ src/main/java/de/tum/in/icm/services/NERPostProcessorService.java
@@ -2,6 +2,7 @@ package de.tum.in.icm.services;
 
 import de.tum.in.icm.dtos.AnnotationDTO;
 import de.tum.in.icm.dtos.NERResultDTO;
+import de.tum.in.icm.dtos.RangeDTO;
 import de.tum.in.icm.entities.TextNodeMap;
 
 import java.util.Map;
@@ -9,30 +10,50 @@ import java.util.Map;
 public class NERPostProcessorService {
 
     private static Map<Integer, Integer> textToNodeIndexMap;
+    private static TextNodeMap textNodeMap;
 
     public static NERResultDTO calculateRangeObjects(NERResultDTO resultDto, TextNodeMap textNodeMap) {
+        NERPostProcessorService.textNodeMap = textNodeMap;
         textToNodeIndexMap = textNodeMap.getTextToNodeIndexMap();
         for (AnnotationDTO annotation : resultDto.getAnnotations()) {
             for (int textIndex : annotation.getPlainTextIndices()) {
-                Integer listIndex = getListIndex(textIndex);
-                int textNodeIndex = getTextNodeIndex(textIndex);
-                int startOffset = textIndex - textNodeIndex;
-                String textNode = textNodeMap.getValues().get(listIndex);
-
-                String startXPath = textNodeMap.getParentLocators().get(listIndex).toString();
-                int relativeStartOffset = textNodeMap.getParentOffsets().get(listIndex) + startOffset;
-                String endXPath = startXPath;
-                int relativeEndOffset = relativeStartOffset + annotation.getValue().length();
-                if (textNode.substring(startOffset).length() < annotation.getValue().length()) {
-                    // annotation value is split over multiple nodes
-                    endXPath = "not implemented";
-                    relativeEndOffset = -1;
-                }
-
-                annotation.addXPathRange(startXPath, relativeStartOffset, endXPath, relativeEndOffset);
+                RangeDTO rangeDTO = calculateRangeDTO(annotation, textIndex);
+                annotation.getRanges().add(rangeDTO);
             }
         }
         return resultDto;
+    }
+
+    private static RangeDTO calculateRangeDTO(AnnotationDTO annotation, int textIndex) {
+        Integer listIndex = getListIndex(textIndex);
+        int textNodeIndex = getTextNodeIndex(textIndex);
+        int startOffset = textIndex - textNodeIndex;
+        String textNode = textNodeMap.getValues().get(listIndex);
+
+        String startXPath = getStartXPath(listIndex);
+        int relativeStartOffset = getRelativeStartOffset(listIndex, startOffset);
+        String endXPath = startXPath;
+        int relativeEndOffset = relativeStartOffset + annotation.getValue().length();
+        if (textNode.substring(startOffset).length() < annotation.getValue().length()) {
+            // annotation value is split over multiple nodes
+            endXPath = "not implemented";
+            relativeEndOffset = -1;
+        }
+
+        RangeDTO rangeDTO = new RangeDTO();
+        rangeDTO.setxPathStart(startXPath);
+        rangeDTO.setxPathEnd(endXPath);
+        rangeDTO.setOffsetStart(relativeStartOffset);
+        rangeDTO.setOffsetEnd(relativeEndOffset);
+        return rangeDTO;
+    }
+
+    private static String getStartXPath(Integer listIndex) {
+        return textNodeMap.getParentLocators().get(listIndex).toString();
+    }
+
+    private static int getRelativeStartOffset(Integer listIndex, int startOffset) {
+        return textNodeMap.getParentOffsets().get(listIndex) + startOffset;
     }
 
     private static int getListIndex(int textIndex) {
