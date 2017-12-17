502bc81def810cb38b53b83fcf83728fb6d72282
Simon Bönisch <github@sunborn.me>
2017-12-17 23:23:06 +0100

    fixed annotation placement

--- src/main/java/de/tum/in/icm/controllers/NERCoreController.java
+++ src/main/java/de/tum/in/icm/controllers/NERCoreController.java
@@ -16,13 +16,13 @@ import javax.ws.rs.core.Response;
 
 
 @Path("/ner")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class NERCoreController {
 
     private NERCoreService nerCoreService = new NERCoreService();
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/recognize")
     public Response recognize(NERInputDTO inputDTO) {
         TextNodeMap textNodeMap = NERPreProcessorService.getTextNodeMap(inputDTO.getHtmlSource());
