d82c2a993f0bbaa278a24f847933b5b93f8699bb
Simon Bönisch <github@sunborn.me>
2018-04-18 14:01:53 +0200

    reimplemented sentence end calculation, yet untested

--- src/main/java/de/tum/in/icm/services/TaskService.java
+++ src/main/java/de/tum/in/icm/services/TaskService.java
@@ -5,14 +5,19 @@ import de.tum.in.icm.dtos.NERType;
 import de.tum.in.icm.dtos.PatternDTO;
 import de.tum.in.icm.dtos.TextOrigin;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskService {
 
-    private static final int MAX_WORDS_COUNT = 20;
+    private static final Logger logger = Logger.getLogger(TaskService.class.getName());
+
+    private static final int MAXIMUM_SENTENCE_LENGTH_WORDS = 20;
+    private static final char[] SENTENCE_STOP_CHARACTERS = {'.', '!', '?'};
 
     public ArrayList<AnnotationDTO> findByUserPatterns(String text, List<PatternDTO> patterns, TextOrigin textOrigin) {
 
@@ -47,7 +52,7 @@ public class TaskService {
                 newMatch.setNerType(NERType.TASK_TITLE);
                 newMatch.setTextOrigin(textOrigin);
                 // pass original text to get words with original letter cases
-                newMatch.setValue(getFullSentence(text, index));
+                newMatch.setValue(getFullSentenceLegacy(text, index));
                 newMatch.addPlainTextIndex(index);
                 result.add(newMatch);
                 index = textInLowerCase.indexOf(pattern.getLabel().toLowerCase(), index + 1);
@@ -57,8 +62,36 @@ public class TaskService {
         return result;
     }
 
+    private String getFullSentence(String text, int index) {
+        int endIndex = getIndexOfFirstStopChar(text.substring(index));
+        String sentence = endIndex == -1 ? text.substring(index) : text.substring(index, index + endIndex);
+        return ensureMaximumSentenceLength(sentence);
+    }
+
+    private int getIndexOfFirstStopChar(String text) {
+        int smallestIndex = Integer.MAX_VALUE;
+        int currentIndex;
+        for (char stopCharacter : SENTENCE_STOP_CHARACTERS) {
+            currentIndex = text.indexOf(stopCharacter);
+            if (currentIndex < smallestIndex)
+                smallestIndex = currentIndex;
+        }
+        return smallestIndex == Integer.MAX_VALUE ? -1 : smallestIndex;
+    }
+
+    private String ensureMaximumSentenceLength(String text) {
+        BreakIterator wordIterator = BreakIterator.getWordInstance();
+        wordIterator.setText(text);
+        for (int i = 0; i < MAXIMUM_SENTENCE_LENGTH_WORDS; i++) {
+            if (wordIterator.next() == BreakIterator.DONE)
+                break;
+        }
+        return text.substring(0, wordIterator.current());
+    }
+
     //returns a whole sentence until we hit the MAX_WORD_COUNT or the sentence ende with '.', '!' or '?'
-    private String getFullSentence(String text, Integer index) {
+    @Deprecated
+    private String getFullSentenceLegacy(String text, Integer index) {
         // TODO simplify by replacing char based processing with usage of indexOf or similar methods
         String result = "";
         int wordsCount = 0;
@@ -66,7 +99,7 @@ public class TaskService {
             return result;
         int currentPos = index;
         char currentChar = text.charAt(currentPos);
-        while (wordsCount < MAX_WORDS_COUNT) {
+        while (wordsCount < MAXIMUM_SENTENCE_LENGTH_WORDS) {
             if (currentChar == '.' || currentChar == '?' || currentChar == '!' || currentChar == '\n')
                 return result;
             if (currentChar == ' ')
